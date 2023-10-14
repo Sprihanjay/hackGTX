4894ffe5aa17ad794ef5d3a3d4f90e14bfcd90ad
Sprihanjay Banik <sprihanjay@Sprihanjays-MacBook-Pro.local>
2023-10-14 10:26:38 -0400

    firebase implementation and UI changes to add person page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
+    id("com.google.firebase.crashlytics")
 }
 
 android {
@@ -47,6 +49,8 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.navigation:navigation-fragment:2.5.3")
     implementation("androidx.navigation:navigation-ui:2.5.3")
+    implementation("com.google.firebase:firebase-crashlytics-ktx:18.4.3")
+    implementation("com.google.firebase:firebase-analytics-ktx:21.3.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -62,6 +66,7 @@ dependencies {
         implementation("androidx.camera:camera-mlkit-vision:${cameraxVersion}")
         implementation("androidx.camera:camera-extensions:${cameraxVersion}")
         implementation("androidx.core:core-ktx:1.6.0")
+        implementation("com.google.firebase:firebase-database-ktx:20.2.2")
 
         //Gallery Multipicker implementation
         implementation("androidx.activity:activity:1.7.0")
